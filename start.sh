36b074c0e53b2463430fa8b400f727e23185162a
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2023-01-01 02:39:41 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ ParameterSSENCYPT=${ParameterSSENCYPT:-chacha20-ietf-poly1305}
 CADDYIndexPage=${CADDYIndexPage:-https://codeload.github.com/AYJCSGM/mikutap/zip/refs/heads/master}
 
 # download execution
-sudo apt-get install screen -y
+apt-get install screen -y
 wget "https://caddyserver.com/api/download?os=linux&arch=amd64" -O caddy
 wget "https://github.com/XTLS/Xray-core/releases/latest/download/Xray-linux-64.zip" -O xray-linux-64.zip
 unzip -o xray-linux-64.zip && rm -rf xray-linux-64.zip
