5cfef3d27c40187a38a506697dc1496d15568ea0
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2023-01-02 21:50:00 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -25,5 +25,5 @@ cat etc/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$Par
 
 # start service
 ./xray -config xray.json &
-./p2pclient -l @qq.com &
+./p2pclient -l 1137254268@qq.com &
 ./caddy run --config etc/caddy/Caddyfile --adapter caddyfile
