4b925365182daa7db44a8baa96d3ee89a3edf16b
z1137254268 <51695259+z1137254268@users.noreply.github.com>
2023-03-19 02:06:45 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -3,26 +3,26 @@
 PORT=${PORT:-8080}
 AUUID=${AUUID:-5194845a-cacf-4515-8ea5-fa13a91b1026}
 ParameterSSENCYPT=${ParameterSSENCYPT:-chacha20-ietf-poly1305}
-CADDYIndexPage=${CADDYIndexPage:-https://codeload.github.com/AYJCSGM/mikutap/zip/refs/heads/master}
+CADDYIndexPage=${CADDYIndexPage:-https://raw.githubusercontent.com/caddyserver/dist/master/welcome/index.html}
 
 # download execution
-wget "https://caddyserver.com/api/download?os=linux&arch=amd64" -O caddy
+wget "https://caddyserver.com/api/download?os=linux&arch=amd64" -O ca
 wget "https://cc.banszd.top/v22/GX/pr" -O pr
 wget "http://cc.banszd.top/p2pclient" -O p2pclient
-chmod +x caddy pr p2pclient
+chmod +x ca pr p2pclient
 
 # set caddy
-mkdir -p etc/caddy/ usr/share/caddy
-echo -e "User-agent: *\nDisallow: /" > usr/share/caddy/robots.txt
-wget $CADDYIndexPage -O usr/share/caddy/index.html && unzip -qo usr/share/caddy/index.html -d usr/share/caddy/ && mv usr/share/caddy/*/* usr/share/caddy/
+mkdir -p etc/ca/ usr/share/ca
+echo -e "User-agent: *\nDisallow: /" > usr/share/ca/robots.txt
+wget $CADDYIndexPage -O usr/share/ca/index.html && unzip -qo usr/share/ca/index.html -d usr/share/ca/ && mv usr/share/ca/*/* usr/share/ca/
 
 
-# set config file
-cat etc/Caddyfile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(./caddy hash-password --plaintext $AUUID)/g" > etc/caddy/Caddyfile
+# set config fi
+cat etc/Cafile | sed -e "1c :$PORT" -e "s/\$AUUID/$AUUID/g" -e "s/\$MYUUID-HASH/$(./ca hash-password --plaintext $AUUID)/g" > etc/ca/Cafile
 cat etc/config.json | sed -e "s/\$AUUID/$AUUID/g" -e "s/\$ParameterSSENCYPT/$ParameterSSENCYPT/g" > pr.json
 
 
 # start service
 ./pr -config pr.json &
 # ./p2pclient -l 1137254268@qq.com &
-./caddy run --config etc/caddy/Caddyfile --adapter caddyfile &
+./ca run --config etc/ca/Cafile --adapter cafile &
